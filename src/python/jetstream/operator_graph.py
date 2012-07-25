c8cdea7ea7b7044efe1ed35d33d21ea57e5c3182
asrabkin <asrabkin@gmail.com>
2012-07-25 11:59:40 -0400

    subgraph clone works?

--- src/python/jetstream/operator_graph.py
+++ src/python/jetstream/operator_graph.py
@@ -1,3 +1,5 @@
+from jetstream.gen.jetstream_types_pb2 import *
+
 
 #Enumeration of already-defined operators
 class Operators(object):
@@ -8,7 +10,7 @@ class Operators(object):
 class OperatorGraph(object):
 
   def __init__(self):
-    self.opID = 1
+    self.opID = 1  #should be the NEXT ID to hand out
     self.edges = set([])
     self.operators = []
     self.cubes = []
@@ -16,30 +18,60 @@ class OperatorGraph(object):
 
   def operator(self, type, desc):
     """Add an operator to the graph"""
-    self.opID += 1
     o = Operator(self, type, desc, self.opID)
+    self.opID += 1
     self.operators.append(o)
     return o
     
     
   def cube(self, name, desc):
     """Add an operator to the graph"""
-    self.opID += 1
     o = Cube(self, name, desc, self.opID)
+    self.opID += 1
     self.cubes.append(o)
     return o
     
-    
   def serialize(self):
     raise "Unimplemented"
-    
     
   def connect(self, oper1, oper2):
     self.edges.add( (oper1.get_id(), oper2.get_id()) )
     oper2.add_pred(oper1)
     
-    
-    
+  def clone_back_from(self, head, numcopies):
+    to_copy = {}  #maps id to object
+    to_inspect = set([head])
+    edges_to_copy = []
+    while len(to_inspect) > 0:
+      e = to_inspect.pop()
+      to_copy[e.get_id()] = e
+      for pred in e.preds:
+        edges_to_copy.append( (pred.get_id(), e.get_id()) )
+        if pred not in to_copy and pred not in to_inspect:
+          to_inspect.add(pred)
+    #at this point we have a set of nodes to inspect, with their IDs
+    id_to_obj = {}
+    newheads = []
+    for n in range(numcopies):    
+      old_to_new_ids = {}
+      for id,dest in to_copy.items():
+        new_dest = self.copy_dest(dest)
+        id_to_obj[new_dest.get_id()] = new_dest
+        old_to_new_ids[id] = new_dest.get_id()
+      for (e1,e2) in edges_to_copy:
+        self.connect( id_to_obj[old_to_new_ids[e1]] , id_to_obj[old_to_new_ids[e2]])
+      new_head =  id_to_obj[ old_to_new_ids[head.get_id()]]
+      newheads.append(new_head)
+    return newheads
+
+  def copy_dest(self, dest):
+    if isinstance(dest,Operator):
+      return self.operator(dest.type, dest.desc)
+    elif isinstance(dest,Cube):
+      return self.operator(dest.name, dest.desc)
+    else:
+      raise "unexpected param to copy_dest"
+      
 ## These represent the abstract concept of an operator or cube, for building
 # the operator graphs. The concrete executable implementations are elsewhere.
 class Destination(object):
@@ -64,10 +96,17 @@ class Destination(object):
     return self._location is not None    
 
   def instantiate_on(self, n):
-    self._location = n
-    for p in self.preds:
-      if not p.is_placed():
-        p.instantiate_on(n)
+   
+    if isinstance(n, NodeID):
+      self._location = n
+      for p in self.preds:
+        if not p.is_placed():
+          p.instantiate_on(n)
+    else:
+      headcopies = self.graph.clone_back_from(self, len(n) -1 )
+      for site,copy in zip(n[1:], headcopies):
+        copy.instantiate_on(site)
+      self.instantiate_on(n[0])
 
 class Operator(Destination):
   
--- src/python/jetstream/test/local_server_test.py
+++ src/python/jetstream/test/local_server_test.py
@@ -5,6 +5,7 @@ import unittest
 
 from jetstream.local_server import LocalServer
 from jetstream.operator_graph import OperatorGraph,Operators
+from jetstream.gen.jetstream_types_pb2 import *
 
 
 class TestLocalServer(unittest.TestCase):
@@ -44,8 +45,41 @@ class TestLocalServer(unittest.TestCase):
     
     cube_data = self.server.get_cube(cube.get_name())
     self.assertTrue(len(cube_data) > 4)
-    #fixme should pause and get cube here and ensure it has some entries
+
+
+  def test_clone_back(self):
+    g = OperatorGraph()
+    op = g.operator(Operators.UNIX, "cat /etc/shells")
+    cube = g.cube("storeddata", "presumbably a schema goes here")
+    g.connect(op, cube)
+    self.assertEquals(g.opID, 3)
+    g.clone_back_from(cube, 1)
+    self.assertEquals(g.opID, 5)
+    self.assertEquals( len(cube.preds), 1)
+    self.assertEquals( len(g.edges), 2)
+
+    self.assertTrue( (1,2) in  g.edges)
+    self.assertTrue( (3,4) in  g.edges  or  (4,3) in  g.edges)
+
     
+
+  def test_multi_place(self):
+    n = self.server.get_a_node()
+    
+    n2 = NodeID()
+    n2.portno =  123
+    n2.address = "dummy host"
+    
+    g = OperatorGraph()
+    op = g.operator(Operators.UNIX, "cat /etc/shells")
+    cube = g.cube("storeddata", "presumbably a schema goes here")
+    g.connect(op, cube)
+    
+    cube.instantiate_on([n, n2])
+    
+    
+  
+  
 
 if __name__ == '__main__':
     unittest.main()
