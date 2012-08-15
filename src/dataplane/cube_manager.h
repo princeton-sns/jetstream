f89b5a4e16c28d5a1c2190368c274d21a7de3498
Mike Freedman <mfreed@cs.princeton.edu>
2012-08-14 22:56:42 -0400

    fixing up some style

--- src/dataplane/cube_manager.h
+++ src/dataplane/cube_manager.h
@@ -1,6 +1,3 @@
-//  Copyright (c) 2012 Princeton University. All rights reserved.
-//
-
 /***
 * Header for the cube manager component. This subsystem of the worker is responsible
 * for allocating, storing, etc the data cubes.
@@ -15,18 +12,19 @@
 
 namespace jetstream {
 
-using namespace::std;
-using namespace boost;
 
 class CubeManager {
-public:
-  CubeManager();
-  shared_ptr<DataCube> get_cube(string s) { return cubeDict[s]; }
-  void put_cube(string s, shared_ptr<DataCube> c) {
-       cubeDict.insert( pair<string, shared_ptr<DataCube> >(s, c) ); }
-private:
-  map<string, shared_ptr<DataCube> > cubeDict;
-  
+ private:
+  std::map<string, shared_ptr<DataCube> > cubeDict;
+
+ public:
+  CubeManager ();
+
+  shared_ptr<DataCube> get_cube (std::string s) { return cubeDict[s]; }
+  void put_cube (std::string s, boost::shared_ptr<DataCube> c) {
+    cubeDict.insert( std::pair<string, boost::shared_ptr<DataCube> >(s, c) ); 
+  }
+
 };
 
 }
