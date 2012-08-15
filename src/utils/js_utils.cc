1392c3186d5b89fbf48acf3ef26a88f7f07d2309
Mike Freedman <mfreed@cs.princeton.edu>
2012-08-14 22:26:03 -0400

    some default boost typedefs in utils

--- src/utils/js_utils.cc
+++ src/utils/js_utils.cc
@@ -1,19 +1,21 @@
 #include "js_utils.h"
 
-timespec tsnow;
-
 #ifdef __MACH__
 #include <mach/clock.h>
 #include <mach/mach.h>
 #endif
 
+using namespace jetstream;
+
+timespec jetstream::tsnow;
+
 void
-jetstream::jetstream_init() {
+jetstream::jetstream_init () {
   refresh_time();
 }
 
 void
-jetstream::refresh_time() {
+jetstream::refresh_time () {
 #ifdef __MACH__ 
   clock_serv_t cclock;
   mach_timespec_t mts;
@@ -28,25 +30,25 @@ jetstream::refresh_time() {
 }
 
 sec_t
-jetstream::get_sec() {
+jetstream::get_sec () {
   return tsnow.tv_sec;
 }
 
 usec_t
-jetstream::get_usec() {
+jetstream::get_usec () {
   refresh_time();
   return ((tsnow.tv_sec * (usec_t) 1000000) + (tsnow.tv_nsec / 1000));
 }
 
 timespec
-jetstream::get_time() {
+jetstream::get_time () {
   refresh_time();
   return tsnow;
 }
 
 /*
 std::string
-jetstream::get_strtime() {
+jetstream::get_strtime () {
   timespec ts = get_time();
   string s2 = str( format("%2% %1%") % 36 % 77 );
   str buf = strbuf("%d.%06d", int (ts.tv_sec), int (ts.tv_nsec / 1000));
