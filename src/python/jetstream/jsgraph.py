89d39e7c53d345e51a5cf9296e1aee046315de31
Siddhartha Sen <sssix@cs.princeton.edu>
2012-10-03 08:26:03 -0400

    Switch to using nodes instead of node ids; fixed bug in LCA alg

--- src/python/jetstream/jsgraph.py
+++ src/python/jetstream/jsgraph.py
@@ -2,35 +2,70 @@
 # Graph data structure and algorithms for representing JetStream queries/computations.
 #
 
+import logging
+from sets import Set
+
 from jetstream_types_pb2 import *
 
-class JSNode(object):
+logger = logging.getLogger('JetStream')
 
+def enum(**enums):
+      return type('Enum', (), enums)
+    
+class JSNode(object):
+  """Internal representation of a node in a JetStream computation graph. JSNode objects
+  should not be returned to an outside caller; return the underlying object instead."""
+
+  # Color enum for graph algorithm
+  class Color:
+    WHITE = 1
+    BLACK = 2
+  
   def __init__ (self, id, obj):
     self.id = id
     self.object = obj
-
     # Declare (and reset) fields used by graph algorithms
     self.reset()
+
     
   def reset (self):
     self.rank = -1
-    self.color = 0
+    self.color = JSNode.Color.WHITE
     self.parent = None
     self.ancestor = None
 
 
+  def __hash__ (self):
+    return hash(self.id) ^ id(self.object)
+
+
+  def __eq__ (self, other):
+    if isinstance(other, JSNode):
+      if (self.id != other.id) or (self.object != other.object):
+        return False
+      return True
+    return NotImplemented
+
+
+  def __ne__ (self, other):
+    result = self.__eq__(other)
+    if result is NotImplemented:
+      return result
+    return not result
+
+
 class JSGraph(object):
+  """A JetStream computation graph"""
 
   def __init__ (self, operators, cubes, edges):
     # Map of node ID -> JSNode
     self.nodes = {}
-    # List of source node IDs (no incoming edge) and final sink node ID
+    # List of source nodes (no incoming edge) and final sink node
     self.sources = []
     self.sink = None
-    # Map of node ID -> [node ID of outgoing neighbor]
+    # Map of node -> [outgoing neighbor]
     self.adjList = {}
-    # Map of node ID -> [node ID of incoming neighbor]
+    # Map of node -> [incoming neighbor]
     self.radjList = {}
 
     for op in operators:
@@ -40,29 +75,30 @@ class JSGraph(object):
 
     # Construct the forward and reverse adjacency lists
     for e in edges:
-      if e.src not in self.adjList:
-        self.adjList[e.src] = []
+      src = self.nodes[e.src]
+      if src not in self.adjList:
+        self.adjList[src] = []
       # Determine if the destination is a cube or operator
-      destId = e.cube_name if e.cube_name != "" else e.dest
-      if destId not in self.radjList:
-        self.radjList[destId] = []
-      self.adjList[e.src].append(destId)
-      self.radjList[destId].append(e.src)
+      dest = self.nodes[e.cube_name] if e.cube_name != "" else self.nodes[e.dest]
+      if dest not in self.radjList:
+        self.radjList[dest] = []
+      self.adjList[src].append(dest)
+      self.radjList[dest].append(src)
 
-    #TODO: How do the checks below interact with validation (when does validation happen?)
-    
     # Determine the source nodes and sink node
-    for node in self.nodes:
+    for node in self.nodes.values():
       if node not in self.radjList:
         self.sources.append(node)
-        print "adding id %d to sources" % (node)
       if node not in self.adjList:
         # There should only be one sink
-        print "adding id %d to sink" % (node)
-        assert(self.sink == None)
+        if self.sink != None:
+          logger.warn("Multiple sinks found in computation graph")
         self.sink = node
     # There should be at least one source
-    assert(self.sources)
+    if len(self.sources) == 0:
+      logger.warn("One or more cycles found in computation graph (were they intended?)")
+
+    # To properly validate a computation graph, use the validate_* methods below
         
 
   def validate_tree (self):
@@ -71,39 +107,48 @@ class JSGraph(object):
 
 
   def lca_sources (self):
-    # TODO: make lcas a set
-    lcas = []
-    lcas.extend(self.sources)
+    """Returns the least-common ancestor (or descendant, more accurately) of the source
+    nodes in the graph. Assumes the graph is (reducible to) a tree."""
+    
+    # Start with all sources and repeatedly compute the LCA of pairs until one LCA
+    # remains. Since LCA(x,x) = x, use a set to track the LCAs.
+    lcas = Set(self.sources)
     lcaPairs = []
     while len(lcas) >= 2:
       lcaPairs.append((lcas.pop(), lcas.pop()))
-    while len(lcaPairs) >= 1:
+    while len(lcaPairs) > 0:
+      # Compute the LCAs of the pairs, starting at the root (aka sink) of the tree
       self.lca_recurse(self.sink, lcaPairs, lcas)
+      # All pairs should be processed and removed by the above call
+      assert len(lcaPairs) == 0
       while len(lcas) >= 2:
         lcaPairs.append((lcas.pop(), lcas.pop()))
     assert(len(lcas) == 1)
-    return lcas[0]
+    return lcas.pop().object
 
 
   def lca_recurse (self, u, lcaPairs, lcas):
-    uNode = self.nodes[u]
-    uf_make_set(uNode)
-    uNode.ancestor = uNode
+    # Clear any prior state
+    u.reset()
+    uf_make_set(u)
+    u.ancestor = u
     # JetStream graphs are directed towards the root, so use the reverse adjacency list.
     if u in self.radjList:
       for v in self.radjList[u]:
         self.lca_recurse(v, lcaPairs, lcas)
-        vNode = self.nodes[v]
-        uf_union(uNode,vNode)
-        uf_find(uNode).ancestor = uNode
-    uNode.color = 1
+        uf_union(u,v)
+        uf_find(u).ancestor = u
+    u.color = JSNode.Color.BLACK
     # Iterate over a copy of the list so we can delete
     for p in lcaPairs[:]:
-      vNode = self.nodes[p[1]]
-      if (p[0] == u) and (vNode.color == 1):
-        lca = uf_find(vNode).ancestor
-        lcas.append(lca.id)
-        print "LCA of %d and %d is %d" % (p[0], p[1], lca.id)
+      # LCA(u,v) is uf_find(v).ancestor immediately after u is colored black, provided
+      # v is already black; otherwise it is uf_find(u).ancestor immediately after v is
+      # colored black.
+      if (p[0] == u) and (p[1].color == JSNode.Color.BLACK):
+        lcas.add(uf_find(p[1]).ancestor)
+        lcaPairs.remove(p)
+      if (p[1] == u) and (p[0].color == JSNode.Color.BLACK):
+        lcas.add(uf_find(p[0]).ancestor)
         lcaPairs.remove(p)
 
 
--- src/python/jetstream/test/jsgraph_test.py
+++ src/python/jetstream/test/jsgraph_test.py
@@ -19,11 +19,11 @@ class TestJSGraph(unittest.TestCase):
 
   def setUp(self):
     # Create a minimal topology as the basis for our tests: some sources connected to
-    # a union operator connected to a sink
+    # a union operator connected to a sink cube
     self.numSources = 5
     self.compId = 1
-    self.operators = [TaskMeta() for i in range(self.numSources + 2)]
-    #self.cubes = [CubeMeta()]
+    self.operators = [TaskMeta() for i in range(self.numSources + 1)]
+    self.cubes = [CubeMeta()]
     self.edges = []
     # The JS graph API only needs ID information from the nodes
     i = 1
@@ -31,21 +31,19 @@ class TestJSGraph(unittest.TestCase):
       op.id.computationID = self.compId
       op.id.task = i
       i += 1
-    #self.cubes[0].name = "unite_sources"
+    self.cubes[0].name = "sink_cube"
     # Connect the sources to the union
     for i in range(self.numSources):
       newEdge = Edge()
       newEdge.computation = self.compId
       newEdge.src = self.operators[i].id.task
-      #newEdge.cube_name = self.cubes[0].name
-      newEdge.dest = self.operators[-2].id.task
+      newEdge.dest = self.operators[-1].id.task
       self.edges.append(newEdge)
-    # Connect the union to the sink
+    # Connect the union to the sink cube
     newEdge = Edge()
     newEdge.computation = self.compId
-    #newEdge.src = self.cubes[0].name
-    newEdge.src = self.operators[-2].id.task
-    newEdge.dest = self.operators[-1].id.task
+    newEdge.src = self.operators[-1].id.task
+    newEdge.cube_name = self.cubes[0].name
     self.edges.append(newEdge)
     
     
@@ -54,39 +52,39 @@ class TestJSGraph(unittest.TestCase):
 
 
   def test_construct(self):
-    graph = JSGraph(self.operators, [], self.edges)
-    # Test sink operator
-    self.assertEquals(graph.sink, self.operators[-1].id.task)
-    self.assertTrue(self.operators[-1].id.task in graph.radjList)
-    self.assertEquals(len(graph.radjList[self.operators[-1].id.task]), 1)
-    self.assertEquals(graph.radjList[self.operators[-1].id.task][0], self.operators[-2].id.task)
-    # Test union operator
-    self.assertTrue(self.operators[-2].id.task in graph.adjList)
-    self.assertEquals(len(graph.adjList[self.operators[-2].id.task]), 1)
-    self.assertEquals(graph.adjList[self.operators[-2].id.task][0], self.operators[-1].id.task)
-    self.assertTrue(self.operators[-2].id.task in graph.radjList)
-    self.assertEquals(len(graph.radjList[self.operators[-2].id.task]), self.numSources)
-#    self.assertEquals(graph.radjList[self.operators[-1].id.task][0], self.cubes[0].name)
-#    self.assertTrue(self.cubes[0].name in graph.adjList)
-#    self.assertEquals(len(graph.adjList[self.cubes[0].name]), 1)
-#    self.assertEquals(graph.adjList[self.cubes[0].name][0], self.operators[-1].id.task)
-#    self.assertTrue(self.cubes[0].name in graph.radjList)
-#    self.assertEquals(len(graph.radjList[self.cubes[0].name]), self.numSources)
+    graph = JSGraph(self.operators, self.cubes, self.edges)
+    # Create JSNode objects to ease comparisons
+    jsnSink = JSNode(self.cubes[0].name, self.cubes[0])
+    jsnUnion = JSNode(self.operators[-1].id.task, self.operators[-1])
+    # Test the sink cube
+    self.assertEquals(graph.sink, jsnSink)
+    self.assertTrue(jsnSink in graph.radjList)
+    self.assertEquals(len(graph.radjList[jsnSink]), 1)
+    self.assertEquals(graph.radjList[jsnSink][0], jsnUnion)
+    # Test the union operator
+    self.assertTrue(jsnUnion in graph.adjList)
+    self.assertEquals(len(graph.adjList[jsnUnion]), 1)
+    self.assertEquals(graph.adjList[jsnUnion][0], jsnSink)
+    self.assertTrue(jsnUnion in graph.radjList)
+    self.assertEquals(len(graph.radjList[jsnUnion]), self.numSources)
+    # Test the sources
     for i in range(self.numSources):
-      self.assertTrue(self.operators[i].id.task in graph.sources)
-      self.assertTrue(self.operators[i].id.task in graph.adjList)
-      self.assertEquals(len(graph.adjList[self.operators[i].id.task]), 1)
-      self.assertEquals(graph.adjList[self.operators[i].id.task][0], self.operators[-2].id.task)
-      self.assertTrue(self.operators[i].id.task in graph.radjList[self.operators[-2].id.task])
-
-    # TODO: validate should pass
+      jsnSource = JSNode(self.operators[i].id.task, self.operators[i])
+      self.assertTrue(jsnSource in graph.sources)
+      self.assertTrue(jsnSource in graph.adjList)
+      self.assertEquals(len(graph.adjList[jsnSource]), 1)
+      self.assertEquals(graph.adjList[jsnSource][0], jsnUnion)
+      self.assertTrue(jsnSource in graph.radjList[jsnUnion])
 
 
   def test_lca_sources(self):
-    graph = JSGraph(self.operators, [], self.edges)
+    graph = JSGraph(self.operators, self.cubes, self.edges)
     lca = graph.lca_sources()
-    # The LCA should be the union operator
-    self.assertEquals(lca, self.operators[-2].id.task)
+    # The LCA of the sources should be the union operator
+    self.assertEquals(lca, self.operators[-1])
+
+    # Create a more complicated graph and find the LCA
+    
 
 if __name__ == '__main__':
   unittest.main()
