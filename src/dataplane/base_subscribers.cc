489495559e34302ccbbea4e08303c256f64b1aa9
Ariel Rabkin <asrabkin@cs.princeton.edu>
2012-10-21 14:46:54 -0400

    rev start scripts; mark start_worker as sns specific

--- src/dataplane/base_subscribers.cc
+++ src/dataplane/base_subscribers.cc
@@ -1,8 +1,34 @@
-#include <string>
+#include "base_subscribers.h"
+#include <glog/logging.h>
 
-#include "subscriber.h"
+using namespace std;
+using namespace boost;
+using namespace jetstream::cube;
 
-using namespace ::std;
+jetstream::cube::Subscriber::Action QueueSubscriber::action_on_tuple(boost::shared_ptr<const jetstream::Tuple> const update) {
+  return returnAction;
+}
+
+void QueueSubscriber::insert_callback(boost::shared_ptr<jetstream::Tuple> const &update, boost::shared_ptr<jetstream::Tuple> const &new_value) {
+  insert_q.push_back(new_value);
+}
+
+void QueueSubscriber::update_callback(boost::shared_ptr<jetstream::Tuple> const &update,boost::shared_ptr<jetstream::Tuple> const &new_value, boost::shared_ptr<jetstream::Tuple> const &old_value) {
+  update_q.push_back(new_value);
+}
+
+jetstream::cube::Subscriber::Action UnionSubscriber::action_on_tuple(boost::shared_ptr<const jetstream::Tuple> const update) {
+  return SEND;
+}
+
+void UnionSubscriber::insert_callback(boost::shared_ptr<jetstream::Tuple> const &update, boost::shared_ptr<jetstream::Tuple> const &new_value) {
+  emit(update);
+}
+
+void UnionSubscriber::update_callback(boost::shared_ptr<jetstream::Tuple> const &update,boost::shared_ptr<jetstream::Tuple> const &new_value, boost::shared_ptr<jetstream::Tuple> const &old_value) {
+  LOG(FATAL)<<"Should never be used";  
+}
+
 
 namespace jetstream {
 
@@ -24,12 +50,25 @@ TimeBasedSubscriber::update_callback(boost::shared_ptr<jetstream::Tuple> const &
   ;
 }
 
+operator_err_t
+TimeBasedSubscriber::configure(std::map<std::string,std::string> &config) {
+ windowSizeMs = 1000;
+}
+
+
+void
+TimeBasedSubscriber::start() {
+  running = true;
+  loopThread = shared_ptr<boost::thread>(new boost::thread(boost::ref(*this)));
+}
 
 void TimeBasedSubscriber::operator()() {
 
 	 while (running)  {
   //sleep
-    boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+    boost::this_thread::sleep(boost::posix_time::milliseconds(windowSizeMs));
+    //should do a query and push the results
+    
 	}
 
 }
@@ -39,4 +78,4 @@ const string TimeBasedSubscriber::my_type_name("Timer-based subscriber");
 
 
 
-}
+} //end namespace
--- src/dataplane/base_subscribers.h
+++ src/dataplane/base_subscribers.h
@@ -1,6 +1,12 @@
 #ifndef Q_SUBSCRIBER_2FAEJ0UJ
 #define Q_SUBSCRIBER_2FAEJ0UJ
 
+#include <boost/shared_ptr.hpp>
+
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
 #include "subscriber.h"
 #include "dataplaneoperator.h"
 #include "jetstream_types.pb.h"
@@ -51,9 +57,9 @@ class UnionSubscriber: public Subscriber {
 class TimeBasedSubscriber: public jetstream::cube::Subscriber {
   private:
     bool running;
-    
-
-
+    boost::shared_ptr<boost::thread> loopThread;
+    int windowSizeMs;
+  
   public:
 
     TimeBasedSubscriber(): running(true) {};
@@ -72,7 +78,12 @@ class TimeBasedSubscriber: public jetstream::cube::Subscriber {
                                  boost::shared_ptr<jetstream::Tuple> const &new_value, 
                                  boost::shared_ptr<jetstream::Tuple> const &old_value);
   
+    virtual operator_err_t configure(std::map<std::string,std::string> &config);
+    virtual void start();
     virtual void stop() {running = false; }
+
+    void operator()();  // A thread that will loop while reading the file
+
 
   private:
   
