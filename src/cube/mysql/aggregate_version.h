2942889dc7f52c63511a4b60363d78e2c04e92aa
Ariel Rabkin <asrabkin@cs.princeton.edu>
2012-12-24 16:11:34 -0500

    code for version aggregate

---
+++ src/cube/mysql/aggregate_version.h
@@ -0,0 +1,54 @@
+//
+//  aggregate_version.h
+//  JetStream
+//
+//  Created by Ariel Rabkin on 12/24/12.
+//  Copyright (c) 2012 Ariel Rabkin. All rights reserved.
+//
+
+#ifndef JetStream_aggregate_version_h
+#define JetStream_aggregate_version_h
+
+#include "aggregate.h"
+
+namespace jetstream {
+namespace cube {
+
+/**
+ * @brief Aggregate for simple counts
+ *
+ * format of input tuples:
+ *
+ * single entry: (none)
+ * partial aggregate: [count of items]
+ */
+class MysqlAggregateVersion: public MysqlAggregate {
+  public:
+    MysqlAggregateVersion() : MysqlAggregate() {};
+
+    vector<string> get_column_types() const;
+
+    string get_update_on_insert_sql() const;
+
+    virtual void insert_default_values_for_full_tuple(jetstream::Tuple &t) const;
+    size_t number_tuple_elements() const;
+  
+    virtual void set_value_for_insert_tuple(
+      shared_ptr<sql::PreparedStatement> pstmt, jetstream::Tuple const &t,
+      int &field_index) const;
+
+    void populate_tuple_final(boost::shared_ptr<jetstream::Tuple> t, boost::shared_ptr<sql::ResultSet> resultset, int &column_index) const ;
+
+    void populate_tuple_partial(boost::shared_ptr<jetstream::Tuple> t, boost::shared_ptr<sql::ResultSet> resultset, int &column_index) const ;
+    
+    virtual string get_select_clause_for_rollup() const;
+  protected:
+    virtual void merge_full_tuple_into(jetstream::Tuple &into, jetstream::Tuple const &update) const;
+
+};
+
+
+} /* cube */
+} /* jetstream */
+
+#endif
