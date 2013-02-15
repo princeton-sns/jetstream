c4197a1a708d401b84bb14c7fb6f31cd9d064074
Matvey Arye <arye@cs.princeton.edu>
2013-02-15 16:36:08 -0500

    more experiment stuff for installing udfs

---
+++ src/mysql_udfs/merge/installdb_linux.sql
@@ -0,0 +1,20 @@
+USE test_cube;
+
+DROP FUNCTION IF EXISTS merge_histogram;
+CREATE AGGREGATE FUNCTION merge_histogram RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+DROP FUNCTION IF EXISTS merge_pair_histogram;
+CREATE FUNCTION merge_pair_histogram RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+
+DROP FUNCTION IF EXISTS merge_reservoir_sample;
+CREATE AGGREGATE FUNCTION merge_reservoir_sample RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+DROP FUNCTION IF EXISTS merge_pair_reservoir_sample;
+CREATE FUNCTION merge_pair_reservoir_sample RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+
+DROP FUNCTION IF EXISTS merge_sketch;
+CREATE AGGREGATE FUNCTION merge_sketch RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+DROP FUNCTION IF EXISTS merge_pair_sketch;
+CREATE FUNCTION merge_pair_sketch RETURNS STRING SONAME 'libjsmysqludfs_merge.so';
+
+
+/* copy dylib to /opt/local/lib/mysql5/mysql/plugin/ */
+
