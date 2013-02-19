4b30bd9fa9df942b00b4839e3fbeda702901cfc8
Siddhartha Sen <sssix@cs.princeton.edu>
2013-02-19 13:00:56 -0500

    Fix some build errors on sns

--- src/python/jetstream/jsgraph.py
+++ src/python/jetstream/jsgraph.py
@@ -136,7 +136,7 @@ class JSGraph (object):
 
 
   #TODO: Use colors to mark visited vertices and avoid redundant exploration
-  # Returns the descendants of a node in topological order, optionally stopping when an
+  #TODO: Returns the descendants of a node in topological order, optionally stopping when an
   # end node is reached.
   def get_descendants (self, startObj, endObj=None):
     start = self.nodes[get_oid(startObj)]
