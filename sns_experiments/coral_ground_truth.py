0bb94678f646e29df7d003a292209377363ec6da
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-02-22 12:16:05 -0500

    coral ground truth upgrade

--- sns_experiments/coral_ground_truth.py
+++ sns_experiments/coral_ground_truth.py
@@ -1,40 +1,61 @@
 from collections import defaultdict
+from optparse import OptionParser 
 
 import csv
 import sys
 import time
 
 def main():
-  infile = sys.argv[1]
+  parser = OptionParser()
+
+  parser.add_option("-t", "--timewindow", dest="timewindow", 
+                  help="time window", default="10")
+  
+  (options, args) = parser.parse_args()
+  infile = args[0]
   print "reading data from %s" % infile
   
-  hour_to_data= {}
+  time_bucket_size = int( options.timewindow)
+  time_to_data= {}
 
   lines = 0
   with open(infile, 'r') as csvfile:
     reader = csv.reader(csvfile, skipinitialspace=True)
+    
     for split_line in reader:
-      hr = to_hour(split_line[1])
-      if not hr in hour_to_data:
-        response_times = defaultdict(int)
-        sizes = defaultdict(int)
-        hour_to_data[hr] = (response_times, sizes)
+      time_bucket = to_timebucket(split_line[1], time_bucket_size)
+      if not time_bucket in time_to_data:
+        response_times, sizes = [],[]
+#        response_times = defaultdict(int)
+#        sizes = defaultdict(int)
+        time_to_data[time_bucket] = (response_times, sizes)
       else:
-        (response_times, sizes) = hour_to_data[hr]
-      update_hist(response_times, int(split_line[-3]))
-      update_hist(sizes, int(split_line[-8]))
+        (response_times, sizes) = time_to_data[time_bucket]
+      update_exact(response_times, int(split_line[-3]))
+      update_exact(sizes, int(split_line[-8]))
       lines += 1
   print "total of %d lines" % lines
   
-  for hr,(response_times,sizes) in hour_to_data.items():
-    print "For hour %s" % time.ctime(hr)
-    print_hist(response_times, "distribution of response times in us")
-    print_hist(sizes, "distribution of file sizes")
+  for t,(response_times,sizes) in sorted(time_to_data.items()):
+    print "For time %s" % time.ctime(t)
+    print_exact(response_times, "distribution of response times in us")
+    print_exact(sizes, "distribution of file sizes")
 
 
-def to_hour(timestamp):
-  timestamp = (int(float(timestamp))  / 3600) * 3600
+def to_timebucket(timestamp, time_bucket_size):
+  timestamp = (int(float(timestamp))  / time_bucket_size) * time_bucket_size
   return timestamp
+  
+
+def update_exact(response_times, val):
+    response_times.append(val)
+  
+QUANTS = [0.05, 0.1, 0.5, 0.9, 0.95]
+def print_exact(a_list, label):
+   a_list.sort()
+   quants = [str(a_list[ int(q * len(a_list))]) for q in QUANTS]
+   q_str = " - ".join( quants )
+   print "%s: %d total items, quantiles are %s" % (label, len(a_list), q_str)
   
 def  update_hist(response_times, t):
   p = 1
@@ -50,7 +71,6 @@ def print_hist(hist, label):
   if len(hist) < 30:
     for (k,v) in sorted(hist.items()):
       print "\t\t%d-%d %d" % (k, k * 2, v)
-      
   return
 
 if __name__ == '__main__':
