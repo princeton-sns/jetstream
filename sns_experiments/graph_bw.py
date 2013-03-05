aa6a654b8cafaed9f51478f7c4cf53adc4a97c27
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-03-05 12:46:13 -0500

    revise metrics reporting

--- sns_experiments/graph_bw.py
+++ sns_experiments/graph_bw.py
@@ -26,11 +26,13 @@ matplotlib.rcParams['pdf.use14corefonts'] = True
 def main():
 
   infile = sys.argv[1]
-  time_to_bw, level_transitions = parse_log(infile)
+  time_to_bw, time_to_tuples, level_transitions = parse_log(infile)
   
   leg_artists = []
-  figure = plot_bw(time_to_bw, leg_artists)  
-  ax = figure.add_subplot(111)
+  figure, ax = plt.subplots()
+  
+  plot_src_tuples(time_to_tuples, ax, leg_artists) 
+  plot_bw(time_to_bw, ax, leg_artists) 
 
   level_transitions = to_line(level_transitions, min(time_to_bw.keys()), max(time_to_bw.keys()))
   
@@ -39,38 +41,41 @@ def main():
 
   finish_plots(figure, ax, leg_artists)
 
-def finish_plots(figure, ax, leg_artists):
-  plt.xlabel('Time', fontsize=24)
-  labels = ax.get_xticklabels() 
-  for label in labels: 
-      label.set_rotation(30)   
-  plt.legend(leg_artists, ["BW", "Degradation"]);
-  
-  if OUT_TO_FILE:
-      plt.savefig("bw_over_time_e1.pdf")
-      plt.close(figure)  
-
-
+USE_BW_REP = False
 def parse_log(infile):
   time_to_bw = {}
   level_transitions = []
+  time_to_tuples = []
   f = open(infile, 'r')
   for ln in f:
-    if 'BWReporter' in ln:
+    if USE_BW_REP and 'BWReporter' in ln:
       fields = ln.split(" ")
       bytes = float(fields[ -4 ])
       tuples = float(fields[ -2 ])
       tstamp = long(fields[-5])
       time_to_bw[tstamp] = (bytes, tuples)
+    elif not USE_BW_REP and 'window@' in ln:
+      fields = ln.split(" ")
+      tstamp = long(fields[7])
+      bytes = float( fields[-2])
+      time_to_bw[tstamp] = (bytes, 0)
+
+#      print zip(fields, range(0, 15))
+#      sys.exit(0)
     elif 'setting degradation level' in ln:
       fields = ln.split(" ")
-      level, ts = int(fields[-5].rstrip(',')), long(fields[-1])
+      level, ts = int(fields[10].rstrip(',')), long(fields[-1])
       level_transitions.append (  (ts, level) )
+    elif 'Tally in window' in ln:
+      fields = ln.split(" ")
+      ts, count = long(fields[-6]), int(fields[-3])     
+      time_to_tuples.append (  (ts, count) )
+       
 
   f.close()
-  return time_to_bw,level_transitions
+  return time_to_bw,time_to_tuples,level_transitions
 
-def plot_bw(time_to_bw, leg_artists):
+def plot_bw(time_to_bw, ax, leg_artists):
   time_data = []
   bw = []
   for tm, (bytes,tuples) in sorted(time_to_bw.items()):
@@ -79,14 +84,25 @@ def plot_bw(time_to_bw, leg_artists):
     bw.append(bytes)
 
   MAX_Y = max(bw)
-
-  fig, ax = plt.subplots()
   
   ax.set_ylim( 0, 1.2 *  MAX_Y)  
   bw_line, = ax.plot_date(time_data, bw, "b.-", label="BW") 
   ax.set_ylabel('BW (bytes)', fontsize=24)
   leg_artists.append( bw_line )
-  return fig
+
+
+def  plot_src_tuples(time_to_count, ax, leg_artists):
+#  print time_to_count
+  
+  counts = [y for (_,y) in time_to_count]
+  times = [datetime.datetime.fromtimestamp(t) for (t,_) in time_to_count]
+  MAX_Y = max(counts)
+  
+  ax.set_ylim( 0, 1.2 *  MAX_Y)  
+  line, = ax.plot_date(times,counts, "g.-") 
+  ax.set_ylabel('Source records/sec', fontsize=24)
+  leg_artists.append( line )  
+  return
   
 
 def to_line(level_transitions, min_time, max_time):
@@ -104,11 +120,25 @@ def plot_degradation(level_transitions, old_ax, leg_artists):
   ax = old_ax.twinx()
   time_data = [datetime.datetime.fromtimestamp(t) for t,l in level_transitions]
   lev_data = [l for t,l in level_transitions]
-  deg_line, = ax.plot_date(time_data, lev_data, "k-", label="Degradation") 
+  deg_line, = ax.plot_date(time_data, lev_data, "k-") 
   ax.set_ylim( 0, 1.2 *  max(lev_data))  
   ax.set_ylabel('Degradation level', fontsize=24)
   leg_artists.append( deg_line )
 #  print level_transitions  
+
+
+
+def finish_plots(figure, ax, leg_artists):
+  plt.xlabel('Time', fontsize=24)
+  labels = ax.get_xticklabels() 
+  for label in labels: 
+      label.set_rotation(30)   
+  plt.legend(leg_artists, ["Src Records", "BW", "Degradation"]);
+  
+  if OUT_TO_FILE:
+      plt.savefig("bw_over_time_e1.pdf")
+      plt.close(figure)  
+
   
 if __name__ == '__main__':
   main()
