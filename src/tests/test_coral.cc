9e1e7ee935432217335ed45b2e291db85524ab5a
Matvey Arye <arye@cs.princeton.edu>
2013-03-05 00:08:14 -0500

    better performance test for coral

--- src/tests/test_coral.cc
+++ src/tests/test_coral.cc
@@ -81,8 +81,8 @@ class TestTupleCoralGenerator {
         summary_dl_time_op->set_dest(receive);
 
       while(tuples.size() < num) {
+        receive->tuples.clear();
         fr_op->emit_1();
-
 
         tuples.insert(tuples.end(), receive->tuples.begin(), receive->tuples.end());
         
@@ -91,7 +91,8 @@ class TestTupleCoralGenerator {
         //create_tuple(*t, time_entered+time_offset+i, "http:\\\\www.example.com", 200, 50, 1);
         //tuples.push_back(t);
       }
-      LOG(INFO) << "Read " << tuples.size() <<" tuples into cubes.";
+      LOG(INFO) << "Read " << tuples.size() <<" tuples. ";
+      //Tuples have "<<(*(tuples.begin()))->e_size() << " Elements. "<<fmt(**(tuples.begin()));
       //LOG(INFO) << "Generated "<< tuples.size() << " tuples. Num= "<<num << " Time started=" << time_entered << "Time ended" << (time_entered+num);
 
     }
@@ -119,6 +120,74 @@ class TestTupleCoralGenerator {
   std::vector< boost::shared_ptr<jetstream::Tuple> > tuples;
 
 };
+
+class TestCSVGenerator {
+
+  public:
+
+    void configure_operator(shared_ptr<DataPlaneOperator> d,  operator_config_t cfg)
+    {
+        operator_err_t err = d->configure(cfg);
+        if (err != NO_ERR) {
+          LOG(FATAL) << "Config Error: " <<err;  
+        }
+    }
+
+    TestCSVGenerator(size_t num) {
+
+        shared_ptr<FileRead> fr_op(new FileRead);
+        operator_config_t cfg;
+        cfg["file"] = CORAL_FILE;
+        cfg["skip_empty"] = "true";
+
+        configure_operator(fr_op, cfg);
+        
+        shared_ptr<DummyReceiver> receive(new DummyReceiver);
+        fr_op->set_dest(receive);
+        
+        while(tuples.size() < num) {
+          fr_op->emit_1();
+          tuples.insert(tuples.end(), receive->tuples.begin(), receive->tuples.end());
+      }
+      LOG(INFO) << "Read " << tuples.size() <<" tuples.";
+    }
+
+    void parse() {
+      unsigned int i = 0;
+
+      shared_ptr<CSVParse> csvp_op(new CSVParse);
+
+      operator_config_t cfg;
+      cfg["discard_off_size"] = "true";
+      cfg["types"] = "IDSSSSSSIIIIIIISS";
+      cfg["fields_to_keep"]="all";
+
+      configure_operator(csvp_op, cfg);
+
+      shared_ptr<DummyReceiver> receive(new DummyReceiver);
+      csvp_op->set_dest(receive);
+      for(std::vector< boost::shared_ptr<jetstream::Tuple> >::const_iterator it = tuples.begin(); it != tuples.end(); ++it) {
+        //LOG(INFO) <<"Inserting: "<< fmt(*(*it));
+        csvp_op->process(*it);
+        ++i;
+
+        LOG_EVERY_N(INFO, 10000) << "CSV Parse at " << i;// <<"Tup " << fmt(*(receive->tuples[0]));
+      }
+      LOG(INFO) << "Finished parsing: " << i << "tuples";
+    }
+
+  protected:
+  std::vector< boost::shared_ptr<jetstream::Tuple> > tuples;
+
+};
+
+
+
+
+
+
+
+
 
 class CoralTest : public ::testing::Test {
   
@@ -193,8 +262,6 @@ void run_test(jetstream::CubeSchema * sc,  unsigned int num_tuples, size_t num_t
 
   LOG(INFO) << "starting timer: "<< start;
 
-  js_usleep(5000);
-  
   boost::thread_group tg;
   for(size_t i = 0; i<gens.size(); ++i) {
     TestTupleCoralGenerator * g= gens[i];
@@ -219,12 +286,48 @@ void run_test(jetstream::CubeSchema * sc,  unsigned int num_tuples, size_t num_t
 
   LOG(INFO) << "Outstanding " << procMon->queue_length() <<"; waits "<< waits << "; start" << start << "; now "<< get_msec();
 
-  LOG(INFO) << "Finished Test num_tuples: "<< num_tuples << " num insert threads: "<< num_tuple_insert_threads<< " num process threads: "<< num_process_threads << ". The time it took was: " << (get_msec() - start);
+  unsigned int diff =  (get_msec() - start);
+  double rate = (double) num_tuples/diff;
 
-
+  LOG(INFO) << "Finished Test num_tuples: "<< num_tuples << " num insert threads: "<< num_tuple_insert_threads<< " num process threads: "<< num_process_threads << ". The time it took was: " << diff << " ms. Rate="<<rate<<" tuples/ms";
 
 }
 
+void run_csv_test(unsigned int num_tuples) {
+  
+  LOG(INFO) << "Running CSV Test num_tuples: "<< num_tuples;
+
+  TestCSVGenerator * g;
+  g = new TestCSVGenerator(num_tuples);
+
+  msec_t start = get_msec();
+
+  LOG(INFO) << "starting timer: "<< start;
+
+  g->parse();
+
+  unsigned int diff =  (get_msec() - start);
+  double rate = (double) num_tuples/diff;
+
+  LOG(INFO) << "Finished Test num_tuples: "<< num_tuples << ". The time it took was: " << diff <<" ms. Rate = "<<rate<<" tuples/ms";
+
+}
+
+
+
 TEST_F(CoralTest, DISABLED_2K14) {
   run_test(sc, 2000, 1, 4);
+}
+
+TEST_F(CoralTest, DISABLED_20K14) {
+  run_test(sc, 20000, 1, 4);
+}
+
+
+TEST_F(CoralTest, DISABLED_CSV20K) {
+  run_csv_test(20000);
+}
+
+TEST_F(CoralTest, DISABLED_CSV200K) {
+  run_csv_test(200000);
 }
