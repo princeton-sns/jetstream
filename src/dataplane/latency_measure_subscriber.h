52550a23336f497b52794d0600aeec0f1efbaeb9
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-03-24 15:06:01 -0400

    use lms in chain

--- src/dataplane/latency_measure_subscriber.h
+++ src/dataplane/latency_measure_subscriber.h
@@ -38,6 +38,7 @@ class LatencyMeasureSubscriber: public jetstream::ThreadedSubscriber {
 
     virtual void process(boost::shared_ptr<Tuple> t) {
       process_c(t);
+      emit(t);
     }
     void process_c(boost::shared_ptr<const Tuple>);
 
