3f8ffd4cdef77f1adfb2f704b93b2b5de05453ab
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-06-10 14:49:45 -0400

    clean up logging

--- src/dataplane/node_daemon.cc
+++ src/dataplane/node_daemon.cc
@@ -59,6 +59,7 @@ parse_config (program_options::variables_map *inputopts,
      "Maximum batch on mysql union selects (as power of 2)")
     ("cube_max_stage", value<u_int16_t>(),
      "Maximum stage of processing")
+   ("data_conn_wait", value<msec_t>(), "wait [in ms] for outgoing data connection before timing out")
     ;
 
 
@@ -147,7 +148,7 @@ parse_config (program_options::variables_map *inputopts,
   if (input_opts.count("cube_max_stage"))
     config.cube_max_stage = input_opts["cube_max_stage"].as<u_int16_t>();
   if (input_opts.count("data_conn_wait"))
-    config.data_conn_wait = input_opts["data_conn_wait"].as<u_int36_t>();
+    config.data_conn_wait = input_opts["data_conn_wait"].as<msec_t>();
 
 
 
