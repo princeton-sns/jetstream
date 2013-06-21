43c900c461e0fc789d26efc08807477bd5041093
Ari Rabkin <asrabkin@gmail.com>
2013-06-20 20:03:48 -0400

    regions file reader

--- src/python/jetstream/regions.py
+++ src/python/jetstream/regions.py
@@ -3,6 +3,16 @@
 import re
 import sys
 
+
+def read_regions(fname):
+  regions = {}
+  srcFile = open(fname, 'r')
+  for ln in srcFile:
+    r_name, r_pat = ln.split(" ")
+    regions[r_name] = re.compile(r_pat)
+  srcFile.close()
+  return regions
+
 def get_1_from_region(region_re, nodelist):
   for n in nodelist:
     if region_re.match(n[0]):
