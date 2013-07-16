972830e3187916bab2f8cd7ef242dd544a9e2234
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-07-16 12:30:48 -0400

    tweak reporting

--- sns_experiments/coral_key_counting.py
+++ sns_experiments/coral_key_counting.py
@@ -36,19 +36,21 @@ def main():
     key = (url, time_bucket)
     key_to_count[key] +=1
     lines += 1
-  print "total of %d lines and %d keys (avg of %0.2f recs per key)" % \
-          (lines, len(key_to_count), float(lines) / len(key_to_count) )
+  recs_per_key = float(lines) / len(key_to_count)
+  print "total of %d lines and %d keys" %  (lines, len(key_to_count),  )
   frequencies = defaultdict(int)
   for count in key_to_count.values():
     frequencies[count] += 1
-  print "%d unique keys (%0.2f%% of total keys)" % \
-    (frequencies[1], frequencies[1] * 100.0 / len(key_to_count))
+  print "%d unique keys (%0.2f%% of total keys) and %0.2f recs per key" % \
+    (frequencies[1], frequencies[1] * 100.0 / len(key_to_count), recs_per_key)
     
   kb_used = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024 - kb_used_start
   elapsed_time = time.time() - start_time
   print "\nAnalysis using %d kbytes in %d seconds" % (kb_used,elapsed_time)
 
 def to_timebucket(timestamp, time_bucket_size):
+  if time_bucket_size == 0:
+    return 0
   timestamp = (int(float(timestamp))  / time_bucket_size) * time_bucket_size
   return timestamp
 
