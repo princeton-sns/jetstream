bd558c185f4321030244c400c28c6339c87df304
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-07-16 11:59:19 -0400

    cleanup more name ambiguities

---
+++ sns_experiments/coral_key_counting.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+from optparse import OptionParser 
+
+import csv
+import resource
+import sys
+import time
+import urlparse
+
+def main():
+  parser = OptionParser()
+
+  parser.add_option("-t", "--timewindow", dest="timewindow", 
+                  help="time window", default="1")
+  parser.add_option("-d", "--domains", dest="domains", action="store_true", default=False,
+                  help="whether to use domains instead of full URLs")
+    
+  (options, args) = parser.parse_args()
+  infile = args[0]
+  time_bucket_size = int( options.timewindow)
+  domains = options.domains
+  
+  print "reading data from %s" % infile
+  kb_used_start = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+  key_to_count = defaultdict(int)
+
+  lines = 0
+  with open(infile, 'r') as csvfile:
+    reader = csv.reader(csvfile, skipinitialspace=True)
+    
+    for split_line in reader:
+      time_bucket = to_timebucket(split_line[1], time_bucket_size)
+      url = split_line[6]
+      if domains:
+        url = url2domain(url) 
+      key = (url, time_bucket)
+      key_to_count[key] +=1
+      lines += 1
+  print "total of %d lines and %d keys (avg of %0.2f recs per key)" % \
+          (lines, len(key_to_count), float(lines) / len(key_to_count) )
+  frequencies = defaultdict(int)
+  for count in key_to_count.values():
+    frequencies[count] += 1
+  print "%d unique keys (%0.2f%% of total keys)" % \
+    (frequencies[1], frequencies[1] * 100.0 / len(key_to_count))
+    
+  kb_used = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024 - kb_used_start
+  print "\nAnalysis using %d kbytes" % kb_used
+
+def to_timebucket(timestamp, time_bucket_size):
+  timestamp = (int(float(timestamp))  / time_bucket_size) * time_bucket_size
+  return timestamp
+
+
+def url2domain(url):
+  u = urlparse.urlparse(url)
+  return u.hostname if u.hostname else url
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
