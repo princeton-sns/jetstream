3cbc8184a44c94b5b968d50b4f42baf4035326e0
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-08-01 11:15:11 -0400

    add image pipe script

---
+++ src/python/jetstream/examples/image_pipe.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+from optparse import OptionParser
+import random
+import sys
+import time
+
+from jetstream_types_pb2 import *
+from remote_controller import *
+import query_graph as jsapi
+from query_planner import QueryPlanner
+
+from coral_util import *   #find_root_node, standard_option_parser,
+
+logger = logging.getLogger('JetStream')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+
+def main():
+
+  parser = standard_option_parser()
+
+  (options, args) = parser.parse_args()
+
+  all_nodes,server = get_all_nodes(options)
+  root_node = find_root_node(options, all_nodes)
+  
+  print "%d worker nodes in system" % len(all_nodes)
+  g= jsapi.QueryGraph()
+  
+  collector = jsapi.AvgCongestLogger(g)
+  collector.instantiate_on(root_node)
+  
+  for node in all_nodes:
+    reader = jsapi.BlobReader(g, dirname="sample_images", prefix="l", ms_per_file="500")
+    filter = jsapi.IntervalSampling(g, max_interval=4)
+    filter.instantiate_on(node)
+    g.chain([reader, filter, collector])
+  print "deploying"
+  deploy_or_dummy(options, server, g)
+
+
+if __name__ == '__main__':
+    main()
