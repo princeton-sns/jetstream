4efcd708bc4dc55151b0be87220b9541fb14030d
Ari Rabkin <asrabkin@gmail.com>
2013-08-14 19:34:58 -0400

    revise lat script to match new file format

--- data_analysis/latency_over_time.py
+++ data_analysis/latency_over_time.py
@@ -27,18 +27,22 @@ def main():
   plot_data_over_time(data)
 
 
-FLD_BW = 6
-FLD_999 = -1
-FLD_MY99 = -6
-FLD_MEDIAN = 14
-
 MEDIAN_LAT = "Median latency"
 MY_LAT = "95th percentile latency"
-FIELDS_TO_PLOT = {
+FIELDS_TO_PLOT_OLD = {
   "BW": 6,
   MEDIAN_LAT: 14,
   MY_LAT: -6,
   "99.9th percentile latency": -1
+}
+
+FIELDS_TO_PLOT = {
+  "Time": 0,
+  "BW": 1,
+  MEDIAN_LAT: 5,
+  MY_LAT: 7,
+  "99.9th percentile latency": 9,
+  "BW-deviation": 11
 }
  
 def parse_infile(infile):
@@ -49,29 +53,26 @@ def parse_infile(infile):
   t = 0
   t_series = []
   for ln in f:
-    if not 'IMGREPORT' in ln:
-      continue
+#    if not 'IMGREPORT' in ln:
+#      continue
       
     fields = ln.strip().split(" ")
     for field,offset in FIELDS_TO_PLOT.items():
-      val = int(fields[offset].strip('.'))
+      val = float(fields[offset].strip('.'))
       data[field].append( val)
-    t +=2
-    
-    t_series.append(t)
   f.close()
-  data['time'] = t_series
   return data
 
 
 def plot_data_over_time(data):
 
-  time = data['time']
+  time = [x / 1000 for x in data['Time']]
   med_series = data[MEDIAN_LAT]
-  bw_series = data['BW']
+  myquant = data[MY_LAT]
+  bw_series = [x / 1000000 for x in data['BW']]
   
   figure, ax = plt.subplots()
-  ax.plot(time, med_series)
+  ax.plot(time, myquant)
   ax2 = ax.twinx()
   ax2.plot(time, bw_series, "r.")  
   
