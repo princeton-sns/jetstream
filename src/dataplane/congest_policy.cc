0ca7edd050873d072d84ccea4bc941d2f3f6e975
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-08-30 17:08:34 -0400

    log rand returns

--- src/dataplane/congest_policy.cc
+++ src/dataplane/congest_policy.cc
@@ -109,10 +109,14 @@ CongestionPolicy::should_upgrade(double capacity,
         max_supported_step ++;
   
   unsigned half_supported = (max_supported_step + curLevel + 1) /2;
-  if (rand() < RAND_MAX * PROB_TO_WAIT_FOR_UPGRADE)
+  int rv = rand();
+  if (rv < RAND_MAX * PROB_TO_WAIT_FOR_UPGRADE) {
+    LOG(INFO) << "Saw rand = " << rv << "/" << (RAND_MAX) << "; jumping up to " << half_supported;
     return half_supported;
-  else
+  } else {
+    LOG(INFO) << "Saw rand = " << rv << "/" << (RAND_MAX) << "; staying put";
     return curLevel;
+  }
 //  double next_step_ratio = ;
 //  if (next_step_ratio )
 //    return curLevel +1;
