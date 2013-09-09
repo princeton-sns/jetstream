9c401eb2fb535b33c1895c48ad6c22a27216a03c
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-09-09 11:32:48 -0400

    shaping kills previous

--- adapt_experiments/background_adapt.sh
+++ adapt_experiments/background_adapt.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+PID_DIR=/jetstream
+
+kill `cat ${PID_DIR}/shaping.pid`
+
 nohup python /jetstream/js/adapt_experiments/wifi_tc_simulate.py > /jetstream/shape.out  2>&1 $@ &
-echo "Shaping."
+PID=$!
+echo $PID > ${PID_DIR}/shaping.pid
+echo "started; pid is ${PID}"
+sleep 1
