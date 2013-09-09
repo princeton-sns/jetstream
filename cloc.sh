a29d26b08e4046e3fa826798bca115c2378c3e54
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-09-09 14:48:27 -0400

    tweak graph layout

--- cloc.sh
+++ cloc.sh
@@ -1,1 +1,2 @@
-perl ~/cloc-1.56.pl src/tests --exclude-list-file=exclude.cloc --by-file
+perl ~/cloc-1.56.pl src --exclude-list-file=exclude.cloc --exclude-ext=txt --by-file
+perl ~/cloc-1.56.pl src --exclude-list-file=exclude.cloc --exclude-ext=txt
