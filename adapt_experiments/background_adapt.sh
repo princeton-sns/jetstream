1e674188a0453d0a5062d27751dc6e57a3ac827f
Ariel Rabkin <asrabkin@cs.princeton.edu>
2013-09-08 23:49:22 -0400

    redirect stdout

--- adapt_experiments/background_adapt.sh
+++ adapt_experiments/background_adapt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup python /jetstream/js/adapt_experiments/wifi_tc_simulate.py  2>&1 $@ &
+nohup python /jetstream/js/adapt_experiments/wifi_tc_simulate.py > /jetstream/shape.out  2>&1 $@ &
 echo "Shaping."
